d25d18f60125dd30acc7c492a5ce2324608d5297
Dennis <honor1016dd@gmail.com>
2022-09-28 05:29:00 +1000

    Set swagger ui docs

--- app.js
+++ app.js
@@ -12,11 +12,15 @@ const connectDB = require('./db/connect');
 const authenticateUser = require('./middleware/authentication');
 
 // Security
-
 const helmet = require('helmet');
 const cors = require('cors');
 const xss = require('xss-clean');
 const rateLimiter = require('express-rate-limit');
+
+// Swagger
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
 
 // Routers
 const authRouter = require('./routes/authRoute');
@@ -47,8 +51,11 @@ app.use(xss());
 
 app.use(fileUpload({ useTempFiles: true }));
 app.get('/', (req, res) => {
-	res.send('Online Cookbook API');
+	res.send(
+		'<h1>Online Cookbook API</h1><a href="/api-docs">Documentation</a>'
+	);
 });
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 // Routes
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/profile', authenticateUser, profileRouter);
