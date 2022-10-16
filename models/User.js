af19ec4cc942528fcc32d382cd25a1127dfa18f1
Dennis <honor1016dd@gmail.com>
2022-10-16 21:21:50 +1100

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const validator = require('validator');
 
 const UserSchema = new mongoose.Schema({
 	name: {
@@ -13,6 +14,10 @@ const UserSchema = new mongoose.Schema({
 	email: {
 		type: String,
 		required: [true, 'Please provide email'],
+		validate: {
+			validator: validator.isEmail,
+			message: 'Please provide valid email',
+		},
 		match: [
 			/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
 			'Please provide a valid email',
