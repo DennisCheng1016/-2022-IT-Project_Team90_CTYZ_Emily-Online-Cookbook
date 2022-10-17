c785c6f5099fd74fd083b35055d44dd277889031
Dennis <honor1016dd@gmail.com>
2022-10-17 18:39:56 +1100

    Update User.js

--- models/User.js
+++ models/User.js
@@ -18,10 +18,6 @@ const UserSchema = new mongoose.Schema({
 			validator: validator.isEmail,
 			message: 'Please provide valid email',
 		},
-		match: [
-			/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-			'Please provide a valid email',
-		],
 		unique: true,
 	},
 	password: {
